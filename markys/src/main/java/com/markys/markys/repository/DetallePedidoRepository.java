11e779dc33604b35848d9dc19da352cfbf158a6c
Oliver Sol Torres Osco <lyanstefano2004@gmail.com>
2025-07-12 13:17:09 -0500

    se agrego una tabla para ventas se cambio detallepedidorepository, admincontroller y repoadmin

--- markys/src/main/java/com/markys/markys/repository/DetallePedidoRepository.java
+++ markys/src/main/java/com/markys/markys/repository/DetallePedidoRepository.java
@@ -2,6 +2,14 @@ package com.markys.markys.repository;
 
 import com.markys.markys.model.DetallePedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Long> {
+
+    @Query("SELECT d.platillo.nombre, SUM(d.cantidad) FROM DetallePedido d GROUP BY d.platillo.nombre")
+    List<Object[]> contarPedidosPorPlatillo();
 }
+
+
